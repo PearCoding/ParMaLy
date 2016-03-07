b221169275567c657586e1651856e9aca4a24916
pearcoding <pearcoding@gmail.com>
2016-03-07 20:18:04 +0100

    Added FOLLOW sets and downgraded to .Net 3.5

--- ParMaLy/Src/Output/SimpleBreakdown.cs
+++ ParMaLy/Src/Output/SimpleBreakdown.cs
@@ -1,7 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ * Copyright (c) 2016, Ömercan Yazici <omercan AT pearcoding.eu>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    3. Neither the name of the copyright owner may be used
+ *       to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+ */
+
+using System;
 using System.Linq;
-using System.Text;
 using System.IO;
 
 namespace PML.Output
@@ -11,8 +39,8 @@ namespace PML.Output
         public static void Print(TextWriter writer, Environment env)
         {
             writer.WriteLine("Simple Breakdown>>");
-            writer.WriteLine("Tokens: " + String.Join(", ", env.Tokens.Select(s => "'" + s + "'")));
-            writer.WriteLine("Groups: " + String.Join(", ", env.Groups.Select(s => s.Name)));
+            writer.WriteLine("Tokens: " + String.Join(", ", env.Tokens.Select(s => "'" + s + "'").ToArray()));
+            writer.WriteLine("Groups: " + String.Join(", ", env.Groups.Select(s => s.Name).ToArray()));
             writer.WriteLine("Start: " + (env.Start == null ? "NOT SET!" : env.Start.Name));
 
             writer.WriteLine("Rules:");
@@ -51,7 +79,16 @@ namespace PML.Output
             foreach (RuleGroup grp in env.Groups)
             {
                 writer.WriteLine("  " + grp.Name + ": { " 
-                    + String.Join(", ", grp.FirstSet.Select(s => (s == null ? "/*EMPTY*/" : "'" + s + "'"))) 
+                    + String.Join(", ", grp.FirstSet.Select(s => (s == null ? "/*EMPTY*/" : "'" + s + "'")).ToArray())
+                    + " }");
+            }
+
+            writer.WriteLine("Follow Sets:");
+            FollowSet.Setup(env);
+            foreach (RuleGroup grp in env.Groups)
+            {
+                writer.WriteLine("  " + grp.Name + ": { "
+                    + String.Join(", ", grp.FollowSet.Select(s => (s == null ? "/*EOF*/" : "'" + s + "'")).ToArray())
                     + " }");
             }
 
--- ParMaLy/Src/Sets/FollowSet.cs
+++ ParMaLy/Src/Sets/FollowSet.cs
@@ -1,18 +1,110 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 2016, Ömercan Yazici <omercan AT pearcoding.eu>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    3. Neither the name of the copyright owner may be used
+ *       to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+ */
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PML
 {
     public static class FollowSet
     {
         // null means End of File!
-        public static List<string> Generate(Environment env, RuleToken token)
+        public static void Setup(Environment env)
         {
-            List<string> output = new List<string>();
-            return output;
+            // First rule (1)
+            // Add EOF token to starting rule.
+            if(env.Start != null)
+            {
+                if (env.Start.FollowSet == null)
+                    env.Start.FollowSet = new List<string>();
+                env.Start.FollowSet.Add(null);
+            }
+
+            // Second rule (2)
+            foreach (Rule r in env.Rules)
+            {
+                if (r.Group.FollowSet == null)
+                    r.Group.FollowSet = new List<string>();
+
+                for (int i = 0; i < r.Tokens.Count - 1; ++i)
+                {
+                    RuleToken t = r.Tokens[i];
+                    if (t.Type == RuleTokenType.Rule)
+                    {
+                        RuleGroup grp = env.GroupByName(t.String);
+                        if (grp.FollowSet == null)
+                            grp.FollowSet = new List<string>();
+
+                        var l = FirstSet.Generate(env, r.Tokens.GetRange(i + 1, r.Tokens.Count - i - 1));
+
+                        foreach (string str in l)
+                        {
+                            if (!grp.FollowSet.Contains(str))
+                                grp.FollowSet.Add(str);
+                        }
+                    }
+                }
+            }
+            
+            // Third rule (3)
+            foreach (Rule r in env.Rules)
+            {
+                for (int i = 0; i < r.Tokens.Count; ++i)
+                {
+                    RuleToken t = r.Tokens[i];
+                    if (t.Type == RuleTokenType.Rule)
+                    {
+                        RuleGroup grp = env.GroupByName(t.String);
+                        if (i == r.Tokens.Count - 1)
+                        {
+                            foreach (string str in r.Group.FollowSet)
+                            {
+                                if (!grp.FollowSet.Contains(str))
+                                    grp.FollowSet.Add(str);
+                            }
+                        }
+                        else
+                        {
+                            var l = FirstSet.Generate(env, r.Tokens.GetRange(i + 1, r.Tokens.Count - i - 1));
+
+                            if (l.Contains(null))
+                            {
+                                foreach (string str in r.Group.FollowSet)
+                                {
+                                    if (!grp.FollowSet.Contains(str))
+                                        grp.FollowSet.Add(str);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
