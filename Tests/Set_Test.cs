212457a2adfe970bfa0ca206c5ab9f111133db56
pearcoding <pearcoding@gmail.com>
2016-03-07 23:39:41 +0100

    Added Follow set test.

--- Tests/Set_Test.cs
+++ Tests/Set_Test.cs
@@ -17,7 +17,7 @@ namespace PML.Tests
             F:	'(' A ')' | ID ;";
 
         [Test]
-        public void Full()
+        public void First()
         {
             Environment env = new Environment(new Logger());
             env.Parse(Test_Source);
@@ -61,6 +61,58 @@ namespace PML.Tests
 
             Assert.Contains("(", F.FirstSet);
             Assert.Contains("ID", F.FirstSet);
+        }
+
+        [Test]
+        public void Follow()
+        {
+            Environment env = new Environment(new Logger());
+            env.Parse(Test_Source);
+            FirstSet.Setup(env);
+            FollowSet.Setup(env);
+
+            RuleGroup A = env.GroupByName("A");
+            RuleGroup B = env.GroupByName("B");
+            RuleGroup S = env.GroupByName("S");
+            RuleGroup T = env.GroupByName("T");
+            RuleGroup F = env.GroupByName("F");
+
+            Assert.NotNull(A);
+            Assert.NotNull(B);
+            Assert.NotNull(S);
+            Assert.NotNull(T);
+            Assert.NotNull(F);
+
+            Assert.NotNull(A.FollowSet);
+            Assert.NotNull(B.FollowSet);
+            Assert.NotNull(S.FollowSet);
+            Assert.NotNull(T.FollowSet);
+            Assert.NotNull(F.FollowSet);
+
+            Assert.AreEqual(A.FollowSet.Count, 2);
+            Assert.AreEqual(B.FollowSet.Count, 2);
+            Assert.AreEqual(S.FollowSet.Count, 3);
+            Assert.AreEqual(T.FollowSet.Count, 3);
+            Assert.AreEqual(F.FollowSet.Count, 4);
+
+            Assert.Contains(null, A.FollowSet);
+            Assert.Contains(")", A.FollowSet);
+
+            Assert.Contains(null, B.FollowSet);
+            Assert.Contains(")", B.FollowSet);
+
+            Assert.Contains(null, S.FollowSet);
+            Assert.Contains("+", S.FollowSet);
+            Assert.Contains(")", S.FollowSet);
+
+            Assert.Contains(null, T.FollowSet);
+            Assert.Contains("+", T.FollowSet);
+            Assert.Contains(")", T.FollowSet);
+
+            Assert.Contains(null, F.FollowSet);
+            Assert.Contains("+", F.FollowSet);
+            Assert.Contains("*", F.FollowSet);
+            Assert.Contains(")", F.FollowSet);
         }
     }
 }
